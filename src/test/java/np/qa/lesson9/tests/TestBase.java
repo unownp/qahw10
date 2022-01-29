2fd7f6e6545013ae2c299581d2d330b75c7c4144
Mark Mark <antiutka@gmail.com>
2022-01-30 00:03:46 +0300

    properties

---
+++ src/test/java/np/qa/lesson9/tests/TestBase.java
@@ -0,0 +1,44 @@
+package np.qa.lesson9.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import np.qa.lesson9.helpers.Attach;
+import np.qa.lesson9.pages.components.FixedBanComponent;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        //Configuration.startMaximized = true;
+        //Configuration.holdBrowserOpen = true;
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
+        String login = System.getProperty("login", "user1");
+        String pass = System.getProperty("pass", "1234");
+
+        Configuration.baseUrl="https://demoqa.com";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
+    }
+    FixedBanComponent fixedBanComponent=new FixedBanComponent();
+
+}
